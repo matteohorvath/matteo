4b1f749816aae99cc46025bbed8c52b4ed968ba5
Andras Horvath <33268751+horvathandrasmate@users.noreply.github.com>
2023-06-30 17:11:57 +0200

    config update

--- blog.config.js
+++ blog.config.js
@@ -3,9 +3,9 @@ const BLOG = {
   author: 'Matteo Horvath',
   email: 'horvathandrasmate@gmail.com',
   link: 'https://matteohorvath.vercel.app',
-  description: 'This is my awesome blog.',
+  description: 'Interviews, math, philosophy. also business. Come, check it out.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'Europe/Zurich', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
+  timezone: 'Europe/Budapest', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
