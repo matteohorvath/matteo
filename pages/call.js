999f1e57bf6262a624ffdc2bfccbb89f73c139aa
Matteo Horvath <horvathandrasmate@gmail.com>
2024-09-15 19:35:50 +0200

    a

---
+++ pages/call.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function Call() {
+  useEffect(() => {
+    window.location.href = "https://calendar.app.google/Ag7WmgffaE6erHSe9";
+  }, []);
+
+  return <></>;
+}
